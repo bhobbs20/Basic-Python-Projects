94b8fb806a14d864198ffb16dae11a778b40f192
Brian Hobbs <brianhobbs216@gmail.com>
2020-02-25 09:17:26 -0500

    first commit

---
+++ programscope.py
@@ -0,0 +1,56 @@
+# Global var
+# name = "brian"
+#
+# def getName():
+#     name ="C#" # local var
+#     print("I an coding with {}".format(name))
+#
+#
+# getName()
+#
+# print(name + " is " + str(len(name)) + " characters long")
+#
+# print("Hello {}, your name is ".format(name) + str(len(name)) + " characters long")
+
+
+# def divide(weight):
+#     if weight % 2 != 0:
+#         print("false")
+#     else:
+#         print("True")
+#
+# divide(2)
+# divide(3)
+# divide(4)
+# divide(5)
+# divide(6)
+#
+
+# lang = ["python", 'C#', 'C++', 'javscript', 'html','css']
+# lang.insert(0, 'java')
+# lang
+# print(lang)
+
+
+# lang.pop(-2)
+# lang.append("sql")
+# print(lang)
+# print(lang[1].upper())
+# lang.append('monkey')
+# print(lang)
+
+# dic_users = {'em_1234': {'fname': 'Brian', 'lname': 'Hobbs', 'email': 'brian@brian.com', 'phone': '978-233-6789'},
+#              'em_1235': {'fname': 'Mike', 'lname': 'Hobbs', 'email': 'mike@brian.com', 'phone': '948-245-6789'},
+#              }
+#
+# print(dic_users['em_1235']['email'])
+
+# i = 0
+# for i in range(10):
+#     print("{} iteration through the loop.".format(i))
+#    i += 1
+
+# i = 0
+# while i < 10:
+#     print("{} iterations through the loop".format(i))
+#     i += 1

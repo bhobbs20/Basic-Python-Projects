59cbdbe26e179c8458e64eea588914972424340f
Brian Hobbs <brianhobbs216@gmail.com>
2020-02-25 10:20:01 -0500

    added some flow control

---
+++ python_if.py
@@ -0,0 +1,48 @@
+# num1 = 12
+# key = True
+#
+# if num1 == 12:
+#     if key:
+#         print("NUm1 is equal to Twelve and they have the key")
+#     else:
+#         print("NUm1 is equal to Twelve and they DONT have the key")
+# elif num1 < 12:
+#     print("NUm1 is less than Twelve")
+# else:
+#     print("NUm1 is not equal to Twelve")
+
+
+# my_sentence = " loves the color"
+#
+# color_list = ['red', 'blue', 'green', 'pink', 'teal', 'black']
+#
+#
+# def color_function(name):
+#     lst = []
+#     for i in color_list:
+#         msg = "{0} {1} {2}".format(name, my_sentence, i)
+#         lst.append(msg)
+#     return lst
+#
+#
+# def get_name():
+#     go = True
+#     while go:
+#         name = input("what is your name?  ")
+#         if name == ' ':
+#             print("You need to enter your name!")
+#         elif name == "Sally":
+#             print("I hate you sally")
+#         else:
+#             go = False
+#     lst = color_function(name)
+#     for i in lst:
+#         print(i)
+#
+#
+# get_name()
+
+# myList = ('Pink','Black','Green','Teal','Red','Blue')
+# for color in myList:
+#     if color == 'Black':
+#         print('The chosen color is Green.')

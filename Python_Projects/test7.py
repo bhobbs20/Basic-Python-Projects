aaa1d6d5d1dbbb3e7606f96d349be8a3b9410064
Brian Hobbs <brianhobbs216@gmail.com>
2020-02-26 17:03:17 -0500

    added I/O lesson

---
+++ Python_Projects/test7.py
@@ -0,0 +1,20 @@
+import os
+
+
+def writeData():
+    data = 'Hello World'
+    with open('text.txt', 'a') as f:
+        f.write(data)
+        f.close()
+
+
+def openFile():
+    with open('text.txt', 'r') as f:
+        data = f.read()
+        print(data)
+        f.close()
+
+
+if __name__ == "__main__":
+    writeData()
+    openFile()

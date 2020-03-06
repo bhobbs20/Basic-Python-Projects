4b1787a14a3bc3a9c9268680c95f6093e4ee38d3
Brian Hobbs <brianhobbs216@gmail.com>
2020-03-06 12:03:29 -0500

    created django project

---
+++ Django/tech_project/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/pigeon/Basic_Python_Projects/Django/tech_project/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

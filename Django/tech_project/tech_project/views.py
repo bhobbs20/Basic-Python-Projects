9e3eb1f153470096e7acd669c2b337be583e8d11
Brian Hobbs <brianhobbs216@gmail.com>
2020-03-07 13:31:43 -0500

    added partials and static files

--- Django/tech_project/tech_project/views.py
+++ Django/tech_project/tech_project/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render
 
 
 def home(request):
-    user = request.user
+    products = ["Cherries", "Apples", "Oranges", "Strawberries", "Pears", "Watermelons"]
     context = {
-        'user': user,
+        'products': products,
     }
     return render(request, "home.html", context)
-
